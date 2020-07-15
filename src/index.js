714d46821e320af07f5ba5459c54cbd842bbb218
Connor Todd <connortodd0304@gmail.com>
2020-07-15 13:46:41 +0100

    update index.js to have axios default url

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import UserContextProvider from './Context/UserContext'
 import WorkshopContextProvider from './Context/WorkshopContext'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://lisbon-js-202003-pjt3-productized-backend.jsrover.wilders.dev';
+console.log(axios.defaults.baseURL)
 
 ReactDOM.render(
   <React.StrictMode>
