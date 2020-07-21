cf451402ad28c8818024c809ac6327c2491a58fd
Tiago Baptista Coelho <tiagojnbaptistacoelho@gmail.com>
2020-07-21 18:06:46 +0100

    uncomented index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-// axios.defaults.baseURL =
-//   'https://lisbon-js-202003-pjt3-productized-backend.jsrover.wilders.dev';
-// console.log(axios.defaults.baseURL);
+ axios.defaults.baseURL =
+   'https://lisbon-js-202003-pjt3-productized-backend.jsrover.wilders.dev';
+ console.log(axios.defaults.baseURL);
 
 
 axios.defaults.withCredentials = true;
